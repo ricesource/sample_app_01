40347ccb76630fa23eb715f8a72e1fb8e39b7953
ricesource <ricenosource@yahoo.co.jp>
2015-06-07 20:38:11 +0900

    Kadai 6-1,2,3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe User do
 
   describe "when emai format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@ba..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -93,4 +93,15 @@ describe User do
       specify {expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAmpLe.CoM" }
+
+    let "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 end
